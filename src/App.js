bf6485c9ac695f37742c660bf9dfded1a5d2d800
marissaellsworth <78607841+marissaellsworth@users.noreply.github.com>
2021-07-09 12:43:57 -0400

    css done kinda almost

--- src/App.js
+++ src/App.js
@@ -34,56 +34,57 @@ function App() {
           </ul>
         </Quotesection>
       </Mainhero>
-      <section className="The-team">
-        <h2 className="Team-heading">Meet the Team</h2>
+      <Meetteamheader>Meet the Team</Meetteamheader>
+      <Meetteam>
         <Name>Jon</Name>
         <Name>Mar</Name>
         <Name>Ron</Name>
-        <Name>Kelsey</Name>
-      </section>
-      <section className="Our-story">
-        <h2 className="Story-heading">Our Story</h2>
+        <Name>Kelsy</Name>
+      </Meetteam>
+      <Ourstory>
+        <Storyheader>Our Story</Storyheader>
         <ul>
           <li>
-            <p className="Story-text">
+            <Storytext>
               Painting is life. That’s what Grandpa Eddy would say watching it
-              dry.
-            </p>
-          </li>
-          <li>
-            <p className="Story-text">
-              “If you want to see the color, you have to put in the paint.”
-            </p>
-          </li>
-          <li>
-            <p className="Story-text">Today, try many colors, paint once.</p>
+              dry. “If you want to see the color, you have to put in the paint.”{" "}
+              <br></br>
+              Today,
+              <br></br>try many colors,
+              <br></br>paint once.
+            </Storytext>
           </li>
         </ul>
-      </section>
+      </Ourstory>
       <footer></footer>
     </div>
   );
 }
+
+// Header start
 
 const Mainheader = styled.header`
   background-color: #5c6bc0;
   height: 241px;
   width: auto;
   padding-left: 15px;
+  padding-right: 15px;
   padding-top: 30px;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  align-items: flex-end;
 `;
 
 const Header = styled.h1`
   color: #ffffff;
   font-size: 24px;
+  text-align: right;
 `;
 
 const Headertwo = styled.h1`
   color: #ffffff;
   font-size: 24px;
+  text-align: right;
 `;
 
 const Startbutton = styled.button`
@@ -93,6 +94,9 @@ const Startbutton = styled.button`
   height: 25px;
   font-size: 12px;
 `;
+
+//Header end
+//Video and Quotes start
 
 const Mainhero = styled.section`
   background-color: #8e99f3;
@@ -139,8 +143,54 @@ const Quotethree = styled.p`
   display: flex;
 `;
 
+//Video and Quotes end
+//Meet the team section start
+
+const Meetteam = styled.section`
+  margin-top: 40px;
+  margin-bottom: 40px;
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+`;
+
+const Meetteamheader = styled.h1`
+  font-size: 36px;
+  display: flex;
+  justify-content: center;
+`;
+
 const Name = styled.h2`
   font-size: 36px;
+  margin: 20px;
+  margin-left: 10px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: space-around;
 `;
+
+//Meet the team section end
+//Our story section start
+
+const Ourstory = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Storyheader = styled.h2`
+  font-size: 36px;
+  display: flex;
+`;
+
+const Storytext = styled.p`
+  font-size: 18;
+  margin: 35px;
+  margin-bottom: 50px;
+  text-align: center;
+`;
+
+//Our story section end
 
 export default App;
