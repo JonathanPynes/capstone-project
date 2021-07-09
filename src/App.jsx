19fdcb00b188d406fa4d00534e4c4615340d8e9d
KELSY FOULKER <kelsy.foulker3@gmail.com>
2021-07-09 13:48:03 -0500

    working on moving over cs changes to main app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -54,35 +54,37 @@ function App() {
   return (
     <div className="App">
       <MyVantaComponent></MyVantaComponent>
-      <section className="Main-hero">
-        <h3>Video here</h3>
-        <ul>
-          <li>
-            <p className="Quote-blocks">
-              Paint once is a great app that gets you lots of kicks with the
-              cool kids
-            </p>
-          </li>
-          <li>
-            <p className="Quote-blocks">
-              Paint once is a great app that gets you lots of kicks with the
-              cool kids
-            </p>
-          </li>
-          <li>
-            <p className="Quote-blocks">
-              Paint once is a great app that gets you lots of kicks with the
-              cool kids
-            </p>
-          </li>
-        </ul>
-      </section>
+      <Mainhero>
+        <Video>Video here</Video>
+        <Quotesection>
+          <ul>
+            <li>
+              <Quoteone>
+                Paint once is a great app that gets you lots of kicks with the
+                cool kids
+              </Quoteone>
+            </li>
+            <li>
+              <Quotetwo>
+                Paint once is a great app that gets you lots of kicks with the
+                cool kids
+              </Quotetwo>
+            </li>
+            <li>
+              <Quotethree>
+                Paint once is a great app that gets you lots of kicks with the
+                cool kids
+              </Quotethree>
+            </li>
+          </ul>
+        </Quotesection>
+      </Mainhero>
       <section className="The-team">
-        <h2 className="Team-heading">Meet the Team</h2>
+        <Meetteamheader>Meet the Team</Meetteamheader>
         <h2 className="Team-names">Jon Mar</h2>
         <h2 className="Team-names">Ron Kelsy</h2>
       </section>
-      <div className="Overlay-section">
+      <Meetteam className="Overlay-section">
         {/* replace buttons with img tags  */}
         <button
           className="primary"
@@ -144,30 +146,158 @@ function App() {
             Back
           </button>
         </Overlay>
-      </div>
+      </Meetteam>
 
-      <section className="Our-story">
-        <h2 className="Story-heading">Our Story</h2>
+      <Ourstory>
+        <Storyheader>Our Story</Storyheader>
         <ul>
           <li>
-            <p className="Story-text">
+            <Storytext>
               Painting is life. That’s what Grandpa Eddy would say watching it
-              dry.
-            </p>
-          </li>
-          <li>
-            <p className="Story-text">
-              “If you want to see the color, you have to put in the paint.”
-            </p>
-          </li>
-          <li>
-            <p className="Story-text">Today, try many colors, paint once.</p>
+              dry. “If you want to see the color, you have to put in the paint.”{" "}
+              <br></br>
+              Today,
+              <br></br>try many colors,
+              <br></br>paint once.
+            </Storytext>
           </li>
         </ul>
-      </section>
+      </Ourstory>
       <footer></footer>
     </div>
   );
 }
+
+// Header start
+
+const Mainheader = styled.header`
+  background-color: #5c6bc0;
+  height: 241px;
+  width: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const Header = styled.h1`
+  color: #ffffff;
+  font-size: 24px;
+  text-align: right;
+`;
+
+const Headertwo = styled.h1`
+  color: #ffffff;
+  font-size: 24px;
+  text-align: right;
+`;
+
+const Startbutton = styled.button`
+  background-color: #ede7f6;
+  border-radius: 30px;
+  width: 72px;
+  height: 25px;
+  font-size: 12px;
+`;
+
+//Header end
+//Video and Quotes start
+
+const Mainhero = styled.section`
+  background-color: #8e99f3;
+  height: 970px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Video = styled.h3`
+height: 356px
+  width: auto;
+  height: 356px;
+  background-color: #ede7f6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Quotesection = styled.section`
+  display: flex;
+  margin-top: 60px;
+`;
+
+const Quoteone = styled.p`
+  margin: 15px;
+  padding: 30px;
+  font-size: 18px;
+  display: flex;
+`;
+
+const Quotetwo = styled.p`
+  margin: 15px;
+  margin-top: 35px;
+  padding: 30px;
+  font-size: 18px;
+  display: flex;
+`;
+
+const Quotethree = styled.p`
+  margin: 15px;
+  margin-top: 35px;
+  padding: 30px;
+  font-size: 18px;
+  display: flex;
+`;
+
+//Video and Quotes end
+//Meet the team section start
+
+const Meetteam = styled.section`
+  margin-top: 40px;
+  margin-bottom: 40px;
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+`;
+
+const Meetteamheader = styled.h1`
+  font-size: 36px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Name = styled.h2`
+  font-size: 36px;
+  margin: 20px;
+  margin-left: 10px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+//Meet the team section end
+//Our story section start
+
+const Ourstory = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Storyheader = styled.h2`
+  font-size: 36px;
+  display: flex;
+`;
+
+const Storytext = styled.p`
+  font-size: 18;
+  margin: 35px;
+  margin-bottom: 50px;
+  text-align: center;
+`;
+
+//Our story section end
 
 export default App;
