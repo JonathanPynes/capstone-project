8059d74240693ab6c116b465c2f4926f45be3083
KELSY FOULKER <kelsy.foulker3@gmail.com>
2021-07-09 23:50:22 -0500

    added text fon into public index file, then added font to all text on marketing page

--- src/App.jsx
+++ src/App.jsx
@@ -176,6 +176,7 @@ const Mainheader = styled.header`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+  font-family: "Noto Sans", sans-serif;
 `;
 
 const Header = styled.h1`
@@ -196,6 +197,7 @@ const Startbutton = styled.button`
   width: 72px;
   height: 25px;
   font-size: 12px;
+  font-family: "Noto Sans", sans-serif;
 `;
 
 //Header end
@@ -206,6 +208,7 @@ const Mainhero = styled.section`
   height: 970px;
   display: flex;
   flex-direction: column;
+  font-family: "Noto Sans", sans-serif;
 `;
 
 const Video = styled.h3`
@@ -255,12 +258,14 @@ const Meetteam = styled.section`
   display: grid;
   grid-template-columns: auto auto;
   justify-content: center;
+  font-family: "Noto Sans", sans-serif;
 `;
 
 const Meetteamheader = styled.h1`
   font-size: 36px;
   display: flex;
   justify-content: center;
+  font-family: "Noto Sans", sans-serif;
 `;
 
 //Meet the team section end
@@ -271,6 +276,7 @@ const Ourstory = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  font-family: "Noto Sans", sans-serif;
 `;
 
 const Storyheader = styled.h2`
